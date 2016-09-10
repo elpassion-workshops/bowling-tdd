0cc1fc82b72ba9725d84a6d109264a2565bf69cc
Dominik Malantowicz <dominik.malantowicz@gmail.com>
2016-09-10 11:35:08 +0200

    Correctly count spare

--- app/src/test/java/pl/elpassion/BowlingGameTest.kt
+++ app/src/test/java/pl/elpassion/BowlingGameTest.kt
@@ -2,6 +2,7 @@ package pl.elpassion
 
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import java.util.*
 
 class BowlingGameTest {
 
@@ -19,6 +20,18 @@ class BowlingGameTest {
         assertEquals(20, game.score())
     }
 
+    @Test
+    fun shouldCorrectlyCountSpare() {
+        game.roll(5)
+        game.roll(5) // spare
+        game.roll(1)
+        (1..17).forEach {
+            game.roll(0)
+        }
+
+        assertEquals(12, game.score())
+    }
+
     private fun rollNPinsTwentyTimes(pins: Int) {
         (1..20).forEach {
             game.roll(pins)
@@ -29,12 +42,23 @@ class BowlingGameTest {
 class BowlingGame {
 
     var score = 0
+    val rolls = ArrayList<Int>()
 
     fun roll(pins: Int) {
-        score += pins
+        rolls.add(pins)
     }
 
     fun score(): Int {
+        var firstInFrame = 0
+        (1..10).forEach {
+            if (rolls[firstInFrame] + rolls[firstInFrame + 1] == 10) {
+                score += 10 + rolls[firstInFrame + 2]
+                firstInFrame += 2
+            } else {
+                score += rolls[firstInFrame] + rolls[firstInFrame + 1]
+                firstInFrame += 2
+            }
+        }
         return score
     }
 
